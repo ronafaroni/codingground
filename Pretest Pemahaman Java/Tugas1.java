73cadeb3306bb4e9734623280b4d4c20a39efa75
ronafaroni <ronafaroni95@gmail.com>
2017-02-27 12:39:46 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas1.java
@@ -0,0 +1,8 @@
+public class Tugas1{
+
+     public static void main(String []args){
+        String nama = "Rona faroni";
+        String kota = "Jepara";
+        System.out.println("Nama Anda : " + nama + ", Kota Anda : " + kota);
+     }
+}
