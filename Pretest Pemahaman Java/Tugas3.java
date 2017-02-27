279c955e5791553a5eac2a908318fa7ccdcfc675
ronafaroni <ronafaroni95@gmail.com>
2017-02-27 12:38:45 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas3.java
@@ -0,0 +1,11 @@
+public class Tugas3{
+    
+    public void tampilAngka(int angka){
+        System.out.println("Angka yang anda masukan adalah " + angka);
+    }
+
+     public static void main(String []args){
+        Tugas3 tgs = new Tugas3();
+        tgs.tampilAngka(3);
+     } 
+}
