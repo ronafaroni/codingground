9a666a0bdb84835b20c64d8b6fa19e15f3b4f741
ronafaroni <ronafaroni95@gmail.com>
2017-02-27 12:39:04 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas4.java
@@ -0,0 +1,12 @@
+public class Tugas4{
+    
+     public static void main(String []args){
+        int x = 3;
+        int s = add(x, 2);
+        System.out.println("Hasilnya adalah " + s);
+     } 
+     
+     public static int add(int a, int b){
+         return a + b;
+     }
+}
