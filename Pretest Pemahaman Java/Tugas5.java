50a42967ef6dda99375d5454736bea940ee1bc90
ronafaroni <ronafaroni95@gmail.com>
2017-02-27 12:39:47 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas5.java
@@ -0,0 +1,10 @@
+public class Tugas5{
+    
+        public static void main(String []args){
+            int a = 5;
+            for(a=5; a>0; a--){
+               System.out.println("Nilai a adalah = " + a); 
+            }
+        } 
+
+}
