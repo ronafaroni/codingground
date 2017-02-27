1bb9047fff8ae3a46d631c312238d942c35582a8
ronafaroni <ronafaroni95@gmail.com>
2017-02-27 12:39:03 +0700

    Created by Coding Ground Client

---
+++ Pretest Pemahaman Java/Tugas2.java
@@ -0,0 +1,12 @@
+public class Tugas2{
+
+     public static void main(String []args){
+        
+    String[] mahasiswa = {"A", "B","C","D"};
+      
+      
+        System.out.println("Nama Mahasiswa 1 : " + mahasiswa[0]);
+        System.out.println("Nama Mahasiswa 2 : " + mahasiswa[1]);
+        System.out.println("Nama Mahasiswa 3 : " + mahasiswa[2]);
+     }
+}
